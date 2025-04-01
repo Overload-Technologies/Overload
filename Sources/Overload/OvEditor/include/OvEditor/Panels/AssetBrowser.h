6eca051059ca1fff017e30092ea74f4fd93f734c
Adrien GIVRY <contact@adrien-givry.com>
2025-03-31 23:48:48 -0400

    Cleaned up asset browser identatations

--- Sources/Overload/OvEditor/include/OvEditor/Panels/AssetBrowser.h
+++ Sources/Overload/OvEditor/include/OvEditor/Panels/AssetBrowser.h
@@ -59,9 +59,6 @@ namespace OvEditor::Panels
 		void ParseFolder(OvUI::Widgets::Layout::TreeNode& p_root, const std::filesystem::directory_entry& p_directory, bool p_isEngineItem, bool p_scriptFolder = false);
 		void ConsiderItem(OvUI::Widgets::Layout::TreeNode* p_root, const std::filesystem::directory_entry& p_entry, bool p_isEngineItem, bool p_autoOpen = false, bool p_scriptFolder = false);
 
-	public:
-		static const std::string __FILENAMES_CHARS;
-
 	private:
 		std::string m_engineAssetFolder;
 		std::string m_projectAssetFolder;

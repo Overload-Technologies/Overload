12fc181dd66a94142b22d473b0593a8ca5e3be5a
Hasan Hüseyin Güneş <hasanxgg1@gmail.com>
2025-12-09 03:48:06 +0300

    Updated assert to be more descriptive (#629)

--- Sources/OvDebug/src/OvDebug/Assertion.cpp
+++ Sources/OvDebug/src/OvDebug/Assertion.cpp
@@ -4,11 +4,28 @@
 * @licence: MIT
 */
 
-#include "OvDebug/Assertion.h"
+#include <OvDebug/Assertion.h>
+#include <OvDebug/Logger.h>
 
 #include <cassert>
+#include <format>
 
-void OvDebug::Assertion::Assert(bool p_condition, const std::string& p_message)
+#if defined(_MSC_VER)
+#   define OV_DEBUG_BREAK() __debugbreak()
+#elif defined(__clang__) || defined(__GNUC__)
+#   define OV_DEBUG_BREAK() __builtin_trap()
+#else
+#   define OV_DEBUG_BREAK() std::abort()
+#endif
+
+void OvDebug::_Assert(const char* p_expression, const std::string_view p_message, const std::source_location& p_location)
 {
-	assert(p_condition && p_message.c_str());
+	OVLOG_ERROR(std::format(
+		"Assertion failed: {}\n"
+		"  Expression: {}\n"
+		"  Function: {}\n"
+		"  Location: {}:{}", 
+		p_message, p_expression, p_location.function_name(), 
+		p_location.file_name(), p_location.line()));
+	OV_DEBUG_BREAK();
 }

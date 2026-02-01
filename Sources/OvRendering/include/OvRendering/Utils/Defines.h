5f755c63bce3a529dba577232e99a9f90d382340
Mun <67842288+MunFaill@users.noreply.github.com>
2026-02-01 02:14:43 -0300

    Modify FORCE_DEDICATED_GPU for Linux environment setup
    Updated FORCE_DEDICATED_GPU macro for Linux to use setenv for GPU selection.

--- Sources/OvRendering/include/OvRendering/Utils/Defines.h
+++ Sources/OvRendering/include/OvRendering/Utils/Defines.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <cstdlib>
+
 #ifdef _WIN32
 #define FORCE_DEDICATED_GPU \
 extern "C"\
@@ -14,11 +16,12 @@ extern "C"\
 	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;\
 }
 #else
-// Linux version - use visibility attribute instead of dllexport
+// Linux version - use setenv
 #define FORCE_DEDICATED_GPU \
-extern "C"\
 {\
-	__attribute__((visibility("default"))) unsigned long NvOptimusEnablement = 0x00000001;\
-	__attribute__((visibility("default"))) int AmdPowerXpressRequestHighPerformance = 1;\
+	setenv("__NV_PRIME_RENDER_OFFLOAD", "1", 1);\
+    setenv("__GLX_VENDOR_LIBRARY_NAME", "nvidia", 1);\
+    setenv("__VK_LAYER_NV_optimus", "NVIDIA_only", 1);\
+	setenv("DRI_PRIME", "1", 1);\
 }
 #endif

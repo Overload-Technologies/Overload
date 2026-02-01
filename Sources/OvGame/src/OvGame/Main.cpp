0aeb07b25fa6d0c822dff5ccb0e373fd0ee1c2cd
Mun <67842288+MunFaill@users.noreply.github.com>
2026-02-01 02:16:10 -0300

    Add FORCE_DEDICATED_GPU for Linux and Windows

--- Sources/OvGame/src/OvGame/Main.cpp
+++ Sources/OvGame/src/OvGame/Main.cpp
@@ -8,20 +8,23 @@
 #include <OvRendering/Utils/Defines.h>
 #include <OvTools/Profiling/TracyAllocators.h>
 
-FORCE_DEDICATED_GPU
-
 #ifdef _DEBUG
 int main()
 #else
 	#ifdef _WIN32
 		#undef APIENTRY
 		#include "Windows.h"
+		FORCE_DEDICATED_GPU
 		INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 	#else
 		int main()
 	#endif
 #endif
 {
+	#ifdef __linux__
+	FORCE_DEDICATED_GPU
+	#endif
+
 	OvGame::Core::Application app;
 	app.Run();
 

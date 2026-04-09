08b4595ba716f1c51c0ea62a469d47fecdb0603c
Gopmyc <hoyaux.gaspard@gmail.com>
2026-04-09 23:41:27 +0200

    Extended model loading with embedded asset extraction

--- Sources/OvRendering/include/OvRendering/Resources/Model.h
+++ Sources/OvRendering/include/OvRendering/Resources/Model.h
@@ -11,6 +11,7 @@
 #include <string>
 #include <optional>
 
+#include "OvRendering/Resources/EmbeddedAssets.h"
 #include "OvRendering/Animation/SkeletalData.h"
 #include "OvRendering/Resources/Mesh.h"
 
@@ -52,6 +53,26 @@ namespace OvRendering::Resources
 		const std::optional<Animation::Skeleton>& GetSkeleton() const;
 
 		/**
+		* Returns embedded material data extracted from the model
+		*/
+		const std::vector<EmbeddedMaterialData>& GetEmbeddedMaterials() const;
+
+		/**
+		* Returns embedded texture data extracted from the model
+		*/
+		const std::vector<EmbeddedTextureData>& GetEmbeddedTextures() const;
+
+		/**
+		* Returns an embedded material data by index
+		*/
+		const EmbeddedMaterialData* GetEmbeddedMaterial(uint32_t p_index) const;
+
+		/**
+		* Returns an embedded texture data by index
+		*/
+		const EmbeddedTextureData* GetEmbeddedTexture(uint32_t p_index) const;
+
+		/**
 		* Returns all animations available on this model
 		*/
 		const std::vector<Animation::SkeletalAnimation>& GetAnimations() const;
@@ -74,6 +95,8 @@ namespace OvRendering::Resources
 	private:
 		std::vector<Mesh*> m_meshes;
 		std::vector<std::string> m_materialNames;
+		std::vector<EmbeddedMaterialData> m_embeddedMaterials;
+		std::vector<EmbeddedTextureData> m_embeddedTextures;
 		std::optional<Animation::Skeleton> m_skeleton;
 		std::vector<Animation::SkeletalAnimation> m_animations;
 

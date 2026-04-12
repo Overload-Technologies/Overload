5d089478fcae52a1e94b506c183b1b40e24441d9
Adrien GIVRY <contact@adrien-givry.com>
2026-04-12 00:22:08 -0400

    Neater asset name display

--- Sources/OvUI/src/OvUI/Widgets/InputFields/AssetField.cpp
+++ Sources/OvUI/src/OvUI/Widgets/InputFields/AssetField.cpp
@@ -4,6 +4,9 @@
 * @licence: MIT
 */
 
+#include <algorithm>
+#include <string>
+
 #include <imgui.h>
 
 #include <OvUI/Widgets/InputFields/AssetField.h>
@@ -18,15 +21,38 @@ void OvUI::Widgets::InputFields::AssetField::_Draw_Impl()
 	const float buttonSize = ImGui::GetFrameHeight();
 	const float innerSize = buttonSize - 2.0f * ImGui::GetStyle().FramePadding.x;
 
+	// Normalize to forward slashes
+	std::string normalized = content;
+	std::replace(normalized.begin(), normalized.end(), '\\', '/');
+
+	// Extract filename for display
+	std::string displayName = normalized;
+	const auto lastSlash = normalized.rfind('/');
+	if (lastSlash != std::string::npos)
+		displayName = normalized.substr(lastSlash + 1);
+
+	// Build tooltip: replace leading ':' with {engine}/ or prepend {project}/
+	std::string tooltip;
+	if (!content.empty() && content != "Empty")
+	{
+		if (content[0] == ':')
+			tooltip = "{engine}/" + normalized.substr(1);
+		else
+			tooltip = "{project}/" + normalized;
+	}
+
 	ImGui::BeginGroup();
 
 	ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x - buttonSize);
 
 	char buf[512] = {};
-	content.copy(buf, sizeof(buf) - 1);
+	displayName.copy(buf, sizeof(buf) - 1);
 	ImGui::BeginDisabled();
 	ImGui::InputText((m_widgetID + "i").c_str(), buf, sizeof(buf), ImGuiInputTextFlags_ReadOnly);
 	ImGui::EndDisabled();
+
+	if (!tooltip.empty() && ImGui::IsItemHovered(ImGuiHoveredFlags_ForTooltip))
+		ImGui::SetTooltip("%s", tooltip.c_str());
 
 	ImGui::SameLine(0, 0);
 

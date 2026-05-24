913247639ef2c9d5954454df9a8830a22a0bbf2a
Gopmyc <hoyaux.gaspard@gmail.com>
2026-05-24 09:03:46 +0200

    feat(ui): add multiline support to InputText widget

--- Sources/OvUI/src/OvUI/Widgets/InputFields/InputText.cpp
+++ Sources/OvUI/src/OvUI/Widgets/InputFields/InputText.cpp
@@ -4,6 +4,7 @@
 * @licence: MIT
 */
 
+#include <cfloat>
 #include <imgui.h>
 
 #include <OvUI/Widgets/InputFields/InputText.h>
@@ -50,8 +51,39 @@ void OvUI::Widgets::InputFields::InputText::_Draw_Impl()
 	if (needFocus)
 		ImGui::SetKeyboardFocusHere(0);
 
-	content.resize(256, '\0');
-	bool enterPressed = ImGui::InputText((label + m_widgetID).c_str(), &content[0], 256, ImGuiInputTextFlags_EnterReturnsTrue | (selectAllOnClick ? ImGuiInputTextFlags_AutoSelectAll : 0));
+	constexpr size_t kSingleLineBufferSize = 256;
+	constexpr size_t kMultilineBufferSize = 4096;
+	const size_t bufferSize = multiline ? kMultilineBufferSize : kSingleLineBufferSize;
+	content.resize(bufferSize, '\0');
+
+	const auto commonFlags = selectAllOnClick ? ImGuiInputTextFlags_AutoSelectAll : ImGuiInputTextFlags_None;
+	bool enterPressed = false;
+
+	if (multiline)
+	{
+		const float fieldHeight = multilineHeight > 0.0f ?
+			multilineHeight :
+			ImGui::GetTextLineHeightWithSpacing() * 4.0f;
+		const float fieldWidth = fullWidth ? -FLT_MIN : 0.0f;
+
+		enterPressed = ImGui::InputTextMultiline(
+			(label + m_widgetID).c_str(),
+			&content[0],
+			bufferSize,
+			ImVec2(fieldWidth, fieldHeight),
+			commonFlags
+		);
+	}
+	else
+	{
+		enterPressed = ImGui::InputText(
+			(label + m_widgetID).c_str(),
+			&content[0],
+			bufferSize,
+			ImGuiInputTextFlags_EnterReturnsTrue | commonFlags
+		);
+	}
+
 	content = content.c_str();
 
 	if (content != previousContent)
@@ -60,6 +92,6 @@ void OvUI::Widgets::InputFields::InputText::_Draw_Impl()
 		this->NotifyChange();
 	}
 
-	if (enterPressed)
+	if (enterPressed && !multiline)
 		EnterPressedEvent.Invoke(content);
 }
